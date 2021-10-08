353afa6ea49c387e03e24e3453ef74e9540cd984
corvinus-r3vqav <jpetra0314@gmail.com>
2021-10-08 10:47:45 +0200

    Create Excel Workbook

--- NegyedikHet_R3VQAV/NegyedikHet_R3VQAV/Form1.cs
+++ NegyedikHet_R3VQAV/NegyedikHet_R3VQAV/Form1.cs
@@ -16,15 +16,50 @@ namespace NegyedikHet_R3VQAV
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp; //Excel alkalmazas
+        Excel.Workbook xlWB; //letrehozunk munkafuzetet
+        Excel.Worksheet xlSheet; //letrehozunk munkalapot a munkafuzeten belul
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         public void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application(); //Excel elinditasa
+                xlWB = xlApp.Workbooks.Add(Missing.Value);//ures munkafuzet letrehozasa
+                xlSheet = xlWB.ActiveSheet; //kivalasztjuk a rajta levo munkalap
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true; //control atadasa a felhasznalonak
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);//ha hiba van, nem kell menteni erre utal a false
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        public void CreateTable()
+        {
+
         }
     }
 }
