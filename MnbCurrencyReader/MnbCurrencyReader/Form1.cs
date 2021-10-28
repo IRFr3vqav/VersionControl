0ba003fff55b00cc0416ec2c9e271ac7f1e53a7f
corvinus-r3vqav <jpetra0314@gmail.com>
2021-10-28 10:49:46 +0200

    Create Diagram()

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MnbCurrencyReader
@@ -24,6 +25,7 @@ namespace MnbCurrencyReader
             WebszolgHivasa();
             dataGridView1.DataSource = Rates;
             XMLfeldolgozasa();
+            Diagram();
         }
 
         private void WebszolgHivasa()
@@ -62,6 +64,25 @@ namespace MnbCurrencyReader
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        private void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
