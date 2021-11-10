245a536c6edec1da2f9aa312d876e211392d1810
corvinus-r3vqav <jpetra0314@gmail.com>
2021-11-10 19:25:28 +0100

    small changes

--- NyolcadikHet_R3VQAV/NyolcadikHet_R3VQAV/Form1.cs
+++ NyolcadikHet_R3VQAV/NyolcadikHet_R3VQAV/Form1.cs
@@ -74,7 +74,7 @@ namespace NyolcadikHet_R3VQAV
             _nextToy = Factory.CreateNew();
             _nextToy.Top = label1.Top + label1.Height + 20;
             _nextToy.Left = label1.Left;
-            Controls.Add(_nextToy);
+            mainPanel.Controls.Add(_nextToy);
         }
 
         private void button3_Click(object sender, EventArgs e)
