ad11bf69711061fb8046cba33c3da31c1e40b594
corvinus-r3vqav <jpetra0314@gmail.com>
2021-12-09 12:30:21 +0100

    Create initial population

--- TizenkettedikHet_R3VQAV/TizenkettedikHet_R3VQAV/Form1.cs
+++ TizenkettedikHet_R3VQAV/TizenkettedikHet_R3VQAV/Form1.cs
@@ -15,6 +15,11 @@ namespace TizenkettedikHet_R3VQAV
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +30,12 @@ namespace TizenkettedikHet_R3VQAV
             //gc.AddPlayer();
             //gc.Start(true);
 
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
 
+            gc.Start();
         }
     }
 }
